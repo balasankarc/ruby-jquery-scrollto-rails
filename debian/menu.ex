38f6b1b8d0eb1596267111c8fd9c4bfc5ad4eac0
Balasankar C <balasankarc@autistici.org>
2015-06-30 22:11:42 +0530

    Imported Debian patch 1.4.3+dfsg-1

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ruby-jquery-scrollto-rails):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="ruby-jquery-scrollto-rails" command="/usr/bin/ruby-jquery-scrollto-rails"
---
+++ debian/ruby-jquery-scrollto-rails.cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the ruby-jquery-scrollto-rails package
+#
+0 4	* * *	root	[ -x /usr/bin/ruby-jquery-scrollto-rails_maintenance ] && /usr/bin/ruby-jquery-scrollto-rails_maintenance
