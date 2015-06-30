c65ecb9080541b28f541dbadb2dff9cfabc834d9
Balasankar C <balasankarc@autistici.org>
2015-06-30 22:30:39 +0530

    Remove unwanted files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ruby-jquery-scrollto-rails):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ruby-jquery-scrollto-rails" command="/usr/bin/ruby-jquery-scrollto-rails"
